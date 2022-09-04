887e1b9289595582bc3d8e41acbe142dc84c6dd1
WilliamAllen <will@wallen.me>
2022-09-03 21:30:44 -0500

    point graph associations

--- src/board/Harbor.h
+++ src/board/Harbor.h
@@ -1,2 +1,7 @@
-/// @brief Types of harbors a point may connect to. Any is a (3 same:1 any) trade discount, brick, lumber, etc. are all (2 type: 1 any) trade discount. 
-static enum HarborType{ANY, NONE, BRICK, LUMBER, ORE, GRAIN, WOOL};
+
+namespace Constants{
+
+    /// @brief Types of harbors a point may connect to. Any is a (3 same:1 any) trade discount, brick, lumber, etc. are all (2 type: 1 any) trade discount. 
+    static enum HarborType{ANY, NONE, BRICK, LUMBER, ORE, GRAIN, WOOL};
+
+}
--- src/board/Point.h
+++ src/board/Point.h
@@ -12,7 +12,7 @@ class Point{
         int connectedTiles[3] = {-1, -1, -1};
 
         /// @brief The type of harbor this point connects to
-        HarborType harborType;
+        Constants::HarborType harborType;
 
         /// @brief Does this point have a settlement
         bool hasSettlement;
@@ -24,7 +24,6 @@ class Point{
         bool hasRoad;
 
     public:
-
         Point(int _index, int _neighboringPoints[3], int _connectedTiles[3]);
 
 
